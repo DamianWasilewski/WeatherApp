7b1a122cb96188644e00b47bfb3c7f7e9fd43ca8
Damian <jujute@wp.pl>
2018-10-27 23:03:11 +0200

    Added dynamically rendered icon in condition field

--- src/components/WeatherTile/WeatherTile.js
+++ src/components/WeatherTile/WeatherTile.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import '../../../node_modules/weather-icons/css/weather-icons.css';
 import './WeatherTile.css';
 
-const WeatherTile = (props) => (
-    <div>
-        {props.city && props.country && props.region && <p>Location: {props.city}, {props.country}, {props.region}</p>}
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
-        {props.humidity && <p>Humidity: {props.humidity}</p>}
-        {props.wind && <p>Wind: {props.wind}</p>}
-        {props.condition && <p>Conditions: {props.condition}</p>}
-    </div>
-);
+class WeatherTile extends Component {
+    render () {
+
+    let conditionIconClassName = `wi wi-day-${this.props.condition}`;
+
+        return (
+            <div>
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.wind && <p>Wind: {this.props.wind}</p>}
+                {this.props.condition && <p>Conditions: {this.props.condition} <i className={conditionIconClassName.toLowerCase()}></i></p>}
+            </div>
+        );
+    }
+}
 
 export default WeatherTile;
